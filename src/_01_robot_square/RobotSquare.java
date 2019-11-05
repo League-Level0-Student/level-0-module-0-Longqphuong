159d869a66ff3c91a97209aaa748259441fff855
545909 <545909@0338S-S-R0B6ZH9.sdcs.local>
2019-11-05 14:27:04 -0800

    Robot Graffiti

--- src/_01_robot_square/RobotSquare.java
+++ src/_01_robot_square/RobotSquare.java
@@ -12,22 +12,25 @@ public class RobotSquare {
     public static void main(String[] args) throws Exception {
  
     	// 1. Make a new Robot
-
+Robot November=new Robot();
 
         // 3. Put the robot's pen down
-
+November.penDown();
 
         // 6. Make the robot move as fast as possible
-
+November.setSpeed(999999999);
 
         // 5. Do everything below here 4 times
 
+for (int i = 0; i < 400; i++) {
+	
 
         //         2. Move your robot 200 pixels
 
-
+November.move(200);
         //         4. Turn the robot 90 degrees to the right (90 degrees)
 
-
+November.turn(90);
+    }
     }
 }
---
+++ src/_02_robot_graffiti/Robot_graffiti.java
@@ -0,0 +1,49 @@
+package _02_robot_graffiti;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Robot_graffiti {
+	public static void main(String[] args) {
+Robot Long= new Robot();
+Long.penDown();
+Long.setSpeed(9999999);
+Long.move(100);
+Long.turn(180);
+Long.move(100);
+Long.turn(-90);
+Long.move(200);
+Long.turn(-90);
+Long.move(100);
+Long.turn(90);
+Long.move(100);
+Long.turn(90);
+Long.move(100);
+Long.turn(90);
+Long.move(200);
+Long.turn(180);
+Long.move(100);
+Long.turn(-90);
+Long.move(150);
+Long.turn(135);
+Long.move(200);
+Long.turn(-135);
+Long.move(200);
+Long.turn(90);
+Long.move(200);
+Long.turn(180);
+Long.move(200);
+Long.turn(-90);
+Long.move(200);
+Long.turn(-90);
+Long.move(150);
+Long.turn(-90);
+Long.move(125);
+Long.turn(-90);
+Long.move(100);
+Long.turn(180);
+Long.move(200);
+
+
+	}
+
+} 
